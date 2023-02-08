0732edf5fb71ac1f484eda8f88a710b5df3c9157
hjf <marinaff-xiii@mail.ru>
2023-02-09 01:06:30 +0500

    HW

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Objects;
+
+public class User {
+    private final String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(login, user.login) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Login - " + login +
+                ", Password - " + password;
+    }
+}
---
+++ src/main/java/org/example/exeption/LoginNotFound.java
@@ -0,0 +1,7 @@
+package org.example.exeption;
+
+public class LoginNotFound extends RuntimeException {
+    public LoginNotFound(String message) {
+    }
+}
+
